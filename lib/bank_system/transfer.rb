4e465340dc66c819c23e3c1f7e54eeabba8f6b99
Sendorf <sendorf@gmail.com>
2015-07-17 18:19:37 +0200

    Created the transfers

--- lib/bank_system/transfer.rb
+++ lib/bank_system/transfer.rb
@@ -1,4 +1,12 @@
 module BankSystem
 	class Transfer
+		attr_reader :sender, :receiver, :amount
+
+		def initialize(sender, receiver, amount)
+			@sender = sender
+			@receiver = receiver
+			@amount = amount
+		end
+
 	end
 end

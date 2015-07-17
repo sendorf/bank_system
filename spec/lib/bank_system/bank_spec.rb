2ae4e703a0ccc5a549b0f6a7e11b137dd729246d
Sendorf <sendorf@gmail.com>
2015-07-17 18:22:32 +0200

    Added test to check that a bank has a trasnfer list

--- spec/lib/bank_system/bank_spec.rb
+++ spec/lib/bank_system/bank_spec.rb
@@ -21,7 +21,30 @@ describe BankSystem::Bank do
         end
         expect(subject.accounts).to eq accounts
       end
+    end
+  end
 
+  describe '.transfers' do
+    context 'Gives the list of transfers of a bank' do
+      it 'The given list is empty' do
+        expect(subject.transfers.empty?).to be true
+      end
+
+      it 'Gives back the transfers list' do
+        expect(subject.transfers).to eq []
+      end
+
+      it 'Gives back the list of transfers already introduced in the bank' do
+        transfers = []
+        4.times do
+          sender = BankSystem::Account.new(rand(10000))
+          receiver = BankSystem::Account.new(rand(10000))
+          amount = rand(sender.balance)
+          transfer = BankSystem::Transfer.new(sender, receiver, amount)
+          transfers << transfer
+        end
+        expect(subject.transfers).to eq transfers
+      end
     end
   end
 end
